b339585838f99e8d7b4d1223d9300cd868baa3f8
JareY07 <jarey1715@gmail.com>
2025-09-29 00:18:01 -0600

    Feat[4]: FE Backend-Integration-Created-User

--- src/features/auth/store/useAuth.ts
+++ src/features/auth/store/useAuth.ts
@@ -1,20 +1,45 @@
+// store/useAuth.ts
 import { getItem, setItem, deleteItemAsync } from 'expo-secure-store';
 import { persist, createJSONStorage } from 'zustand/middleware';
-import { type UserState } from '@/types/authTypes';
+import { type UserState, type SignUpData } from '@/types/authTypes';
 import { create } from 'zustand';
+
+const initialSignUpData: SignUpData = {
+  firstName: '',
+  lastName: '',
+  username: '',
+  userMail: '',
+  userPassword: '',
+};
 
 export const useAuthStore = create(
   persist<UserState>(
-    (set) => ({
+    (set, get) => ({
       isLoggedIn: false,
       authFlow: null,
       tempEmail: null,
+      signUpData: initialSignUpData,
+
       logIn: () => set({ isLoggedIn: true, authFlow: null }),
-      logOut: () => set({ isLoggedIn: false, authFlow: null, tempEmail: null }),
+      logOut: () =>
+        set({
+          isLoggedIn: false,
+          authFlow: null,
+          tempEmail: null,
+          signUpData: initialSignUpData,
+        }),
+
       setAuthFlow: (flow) => set({ authFlow: flow }),
       setTempEmail: (email) => set({ tempEmail: email }),
       clearTempEmail: () => set({ tempEmail: null }),
       resetAuthFlow: () => set({ authFlow: null, tempEmail: null }),
+
+      setSignUpData: (data: Partial<SignUpData>) =>
+        set((state) => {
+          const updatedData = { ...state.signUpData, ...data };
+          return { signUpData: updatedData };
+        }),
+      clearSignUpData: () => set({ signUpData: initialSignUpData }),
     }),
     {
       name: 'auth-store',
